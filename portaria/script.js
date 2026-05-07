b632990a5841529cdd3a811a7de3ca7b9dea18bd
Kinike <86970091+gusguela@users.noreply.github.com>
2026-05-07 07:56:00 -0300

    Add files via upload

---
+++ portaria/script.js
@@ -0,0 +1,5 @@
+function check(){
+    var wasborn = parseInt(document.getElementById('birth').value)
+    var res = document.getElementById('result')
+    var clientage = document.getElementById('age')
+}
